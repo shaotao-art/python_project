9f3170d3970b6d76422b55cb766e65736718253a
starfish <2842226412@qq.com>
2021-03-31 13:44:59 +0800

    更新不同长度字符串的处理

--- little_tools/str_compare.py
+++ little_tools/str_compare.py
@@ -2,12 +2,24 @@ class str_compare_print():
     def __init__(self,str1,str2):
         self.str1=str1
         self.str2 = str2
+        self.length1=len(str1)
+        self.length2= len(str2)
+        self.duode_part=self._get_duode_part()
         self.run()
 
+        #获取长度更大的字符串的多的部分
+    def _get_duode_part(self):
+        if self.length1>self.length2:
+            duode_part=self.str1[self.length2:]
+        else:
+            duode_part = self.str2[self.length1:]
+        return duode_part
 
+
+        #比较字符串并且记录不同字符处的索引
     def _Str_compare(self):
         index_lst=[]
-        length=max((len(self.str1),len(self.str2)))
+        length=min((len(self.str1),len(self.str2)))
         i=0
         while i < length:
             if self.str1[i]!=self.str2[i]:
@@ -21,6 +33,8 @@ class str_compare_print():
             else:
                 i+=1
         return index_lst
+
+    #根据索引对字符串进行更新  并将不同部分高亮输出
     def _Output(self,index_lst,str_out):
         #为了插入改变idex_lst值
         for i in range(len(index_lst)):
@@ -33,15 +47,19 @@ class str_compare_print():
                 str_out.insert(each,'\033[1;37;41m')
             else:
                 str_out.insert(each, '\033[0m')
+        str_out.append('\033[1;37;41m')
+        str_out.append(self.duode_part)
+        str_out.append('\033[0m')
         res=''.join(each for each in str_out)
         print(res)
+
     def run(self):
         index_lst = self._Str_compare()
         self._Output(index_lst, self.str1)
 
 
-str1='abcde123fg779hgh'
-str2='abcde243fg888hii'
+str1='https://s.taobao.com/search?spm=a21bo.21814703.201856-fline.2.5af911d9BrSph5&q=%E5%9B%9B%E4%BB%B6%E5%A5%97&refpid=420461_1006&source=tbsy&style=grid&tab=all&pvid=d0f2ec2810bcec0d5a16d5283ce59f67&bcoffset=0&p4ppushleft=3%2C44&s=88'
+str2='https://s.taobao.com/search?spm=a21bo.21814703.201856-fline.2.5af911d9BrSph5&q=%E5%9B%9B%E4%BB%B6%E5%A5%97&refpid=420461_1006&source=tbsy&style=grid&tab=all&pvid=d0f2ec2810bcec0d5a16d5283ce59f67&bcoffset=0&p4ppushleft=3%2C44&sort=credit-desc'
 
 a=str_compare_print(str1,str2)
 
